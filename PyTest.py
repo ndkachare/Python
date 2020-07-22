74c4d5a6cbcd31fe1bcbdf28097e36406a907232
narendra <narendra1811.kachare@gmail.com>
2020-07-22 01:37:09 -0400

    logical operator

--- PyTest.py
+++ PyTest.py
@@ -1,9 +1,9 @@
 name = input("What is your name? ")
 name_length = len(name)
 print(name_length)
-if name_length <3 and name_length <=50:
+if name_length <3:
     print("Name must be at least 3 characters")
-elif name_length >50 and name_length > 3:
+elif name_length >50:
     print("Name must be a maximum of 50 characters")
 else:
     print("Name looks good")
