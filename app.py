1413f017ad3af3a188eb6817c809cc4e8d234a3f
narendra <narendra1811.kachare@gmail.com>
2020-07-29 23:45:05 -0400

    correction

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ from shoppingCart import find_max
 
 print(converters.kg_to_lb(100))
 numbers = [10,15,50,52,20]
-max = find_max(numbers)
-print(max)
+# max = find_max(numbers)  it a bad practice as changing the meaning of a built in function
+maximum = find_max(numbers)
+print(maximum)
