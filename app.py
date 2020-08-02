60e0d67fbd94d702f39aa237b4dcf4e9bd0ad8c8
narendra <narendra1811.kachare@gmail.com>
2020-08-02 00:39:57 -0400

    Buit-in Function

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import converters
 from shoppingCart import find_max
+# import ecommerce.shipping
+from ecommerce import shipping
+import random
 
+
+shipping.calc_shipping()
 
 print(converters.kg_to_lb(100))
 numbers = [10,15,50,52,20]
